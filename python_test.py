3a58f67f80de0b5e529da1abdbd1001d51418d72
hitdataexp <semudeep@gmail.com>
2017-04-21 10:31:53 -0500

    Sensor Data insert API  added

--- python_test.py
+++ python_test.py
@@ -1,7 +1,7 @@
 import requests
 url = 'http://10.10.46.142:3001/insertdata'
 #HALL_SENSOR,MSTRM_SENSOR,GSTRM_SENSOR
-payload = {'userId': 'admin','password': 'admin', 'sensorId': 'GSTRM_SENSOR', 'month': 3, 'day': 17, 'hour': 3, 'minute': 07, 'second': 0}
+payload = {'userId': 'admin','password': 'admin', 'sensorId': 'GSTRM_SENSOR'}
 
 r = requests.post(url, data=payload)
 print(r.status_code)
