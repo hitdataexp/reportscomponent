4f682a819009199dd15a149d263c5fa985b53d27
apesconsole <apesconsole@gmail.com>
2017-05-11 12:00:38 -0500

    heroku build
    heroku build

--- python_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = 'http://10.10.46.142:3001/insertdata'
-#HALL_SENSOR,MSTRM_SENSOR,GSTRM_SENSOR
-payload = {'userId': 'admin','password': 'admin', 'sensorId': 'GSTRM_SENSOR'}
-
-r = requests.post(url, data=payload)
-print(r.status_code)
